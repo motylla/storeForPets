67a1e54cac423c13aaeec7059c9c3375852a85b5
motylla <magda.waliszewska@gmail.com>
2019-07-14 09:50:53 +0200

    first commit

---
+++ src/main/java/pl/sda/storeforpets/StoreforpetsApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.storeforpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreforpetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreforpetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/storeforpets/StoreforpetsApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.storeforpets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreforpetsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
