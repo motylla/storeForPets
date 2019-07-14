a5f3ef3f71e0005c8baede975593db0bf7aabe85
motylla <magda.waliszewska@gmail.com>
2019-07-14 11:55:31 +0200

    Class Cart added

---
+++ src/main/java/pl/sda/storeforpets/model/Cart.java
@@ -0,0 +1,24 @@
+package pl.sda.storeforpets.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    private long id;
+    private Item item;
+    private LocalDateTime orderDate;
+    private BigDecimal price;
+    private BigDecimal discount;
+    private int quantity;
+    private BigDecimal sumPrice;
+
+}
---
+++ src/main/java/pl/sda/storeforpets/model/Item.java
@@ -0,0 +1,4 @@
+package pl.sda.storeforpets.model;
+
+public class Item {
+}
